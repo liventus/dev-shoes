962122aaa1cbddc43994abe59254554c0412a22f
daniel <daniel.lizana01@gmail.com>
2024-07-02 02:39:52 -0500

    version estable login culminado

--- app/src/main/java/com/example/dev_shoes/MainActivity.kt
+++ app/src/main/java/com/example/dev_shoes/MainActivity.kt
@@ -3,95 +3,18 @@ package com.example.dev_shoes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.example.dev_shoes.components.*
 import com.example.dev_shoes.navigation.NavManager
-import com.example.dev_shoes.ui.theme.DevshoesTheme
-import com.example.dev_shoes.views.DetailView
-import com.example.dev_shoes.views.HomeView
+
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel : MainViewModel by viewModels()
+
         setContent {
-            //NavManager() ultimo que toy desarrollando
-            DevshoesTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Content(viewModel)
-                }
-            }
+            NavManager() //ultimo que toy desarrollando
+
         }
-    }
-}
-
-@Composable
-fun Content(viewModel: MainViewModel) {
-    Column (
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        BotonColor()
-        if(viewModel.isLoading){
-            CircularProgressIndicator()
-        } else {
-            Text(text = viewModel.resultState)
-        }
-
-
-
-        Button(onClick = { viewModel.fetchData() }) {
-            Text(text = "Llamar API")
-        }
-
-    }
-}
-
-@Composable
-fun BotonColor(){
-    var color by remember { mutableStateOf(false)}
-    
-    Button(onClick = { color = !color } , colors = ButtonDefaults.buttonColors(
-        containerColor =  if(color) Color.Blue else Color.Red
-    )) {
-        Text("Cambiar color")
     }
 }
 
--- app/src/main/java/com/example/dev_shoes/views/LoginView.kt
+++ app/src/main/java/com/example/dev_shoes/views/LoginView.kt
@@ -4,24 +4,37 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.navigation.NavController
+import com.example.dev_shoes.R
 import com.example.dev_shoes.components.MainButton
 import com.example.dev_shoes.components.MainIconButton
 import com.example.dev_shoes.components.SpaceH
 import com.example.dev_shoes.components.TitleBar
 import com.example.dev_shoes.components.TitleView
+
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,14 +44,14 @@ fun LoginView(navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
-                title = { TitleBar(name = "LOG IN") },
+                title = { TitleBar(name = "Login") },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = Color.Red
                 ),
                 navigationIcon = {
                     MainIconButton(icon = Icons.Default.ArrowBack) {
                         navController.popBackStack()
-                        
+
                     }
                 }
 
@@ -49,10 +62,16 @@ fun LoginView(navController: NavController) {
     }
 
 
-
 }
+
 @Composable
-fun ContentLoginView(navController: NavController){
+fun ContentLoginView(navController: NavController) {
+
+    val passwordVector = painterResource(id = R.drawable.password_eye)
+    val emailValue = remember { mutableStateOf("") }
+    val passwordValue = remember { mutableStateOf("") }
+    val passwordVisibility = remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -62,15 +81,53 @@ fun ContentLoginView(navController: NavController){
         TitleView(name = "INICIAR SESION")
         SpaceH()
 
-         TextField(
-          value = "opcional",
-          onValueChange = { },
-          label = { Text(text = "opcional") }
-      )
+       /* TextField(
+            value = "opcional",
+            onValueChange = { },
+            label = { Text(text = "opcional") }
+        )*/
 
+
+
+        OutlinedTextField(
+            value = emailValue.value,
+            onValueChange = { emailValue.value = it },
+            label = { Text(text = "Phone") },
+            placeholder = { Text(text = "Enter the phone number") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(0.8f),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Phone)
+        )
+
+        SpaceH()
+        OutlinedTextField(
+            value = passwordValue.value,
+            onValueChange = { passwordValue.value = it },
+            trailingIcon = {
+                IconButton(onClick = {
+                    passwordVisibility.value = !passwordVisibility.value
+                }) {
+                    Icon(
+                        painter = passwordVector, contentDescription = "Password icon",
+                        tint = if (passwordVisibility.value) Color.Red else Color.Gray
+                    )
+                }
+            },
+            label = { Text(text = "Password") },
+            placeholder = { Text(text = "Enter the password") },
+            singleLine = true,
+            visualTransformation = if (passwordVisibility.value) VisualTransformation.None
+            else PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth(0.8f)
+        )
+        SpaceH()
+        SpaceH()
         MainButton(name = "INICIAR SESION", backColor = Color.Red, color = Color.White) {
             navController.navigate("MenuPrincipal")
         }
 
+
     }
+
+
 }
